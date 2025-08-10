908785429c5b84874a219530c64b57a43f738653
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-08-11 00:40:25 +0200

    restore proper paper doll rendering on Fabric

--- Common/src/main/java/fuzs/paperdoll/config/DisplayAction.java
+++ Common/src/main/java/fuzs/paperdoll/config/DisplayAction.java
@@ -2,45 +2,130 @@ package fuzs.paperdoll.config;
 
 import net.minecraft.world.entity.player.Player;
 
-import java.util.function.Predicate;
-
 public enum DisplayAction {
-    SPRINTING(Player::canSpawnSprintParticle, (ClientConfig.DisplayActionsConfig config) -> config.sprinting),
-    SWIMMING(Predicate.not(Player::isVisuallyCrawling)
-            .and(Player::isVisuallySwimming)
-            .and((Player player) -> player.getSwimAmount(1.0F) > 0.0F),
-            (ClientConfig.DisplayActionsConfig config) -> config.sprinting),
-    CRAWLING(Player::isVisuallyCrawling, (ClientConfig.DisplayActionsConfig config) -> config.crawling),
-    CROUCHING(Player::isCrouching, (ClientConfig.DisplayActionsConfig config) -> config.crouching),
-    FLYING((Player player) -> player.getAbilities().flying,
-            (ClientConfig.DisplayActionsConfig config) -> config.flying),
-    GLIDING(Player::isFallFlying, (ClientConfig.DisplayActionsConfig config) -> config.gliding),
-    RIDING(Player::isPassenger, (ClientConfig.DisplayActionsConfig config) -> config.riding),
-    SPIN_ATTACKING(Player::isAutoSpinAttack, (ClientConfig.DisplayActionsConfig config) -> config.spinAttacking),
-    USING(Player::isUsingItem, (ClientConfig.DisplayActionsConfig config) -> config.using);
+    SPRINTING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.sprinting;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.canSpawnSprintParticle();
+        }
+    },
+    SWIMMING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.swimming;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return !player.isVisuallyCrawling() && player.isVisuallySwimming() && player.getSwimAmount(1.0F) > 0.0F;
+        }
+    },
+    CRAWLING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.crawling;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isVisuallyCrawling();
+        }
+    },
+    CROUCHING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.crouching;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player, int remainingRidingTicks) {
+            return this.isPerformingAction(player);
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isCrouching();
+        }
+    },
+    FLYING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.flying;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.getAbilities().flying;
+        }
+    },
+    GLIDING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.gliding;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isFallFlying();
+        }
+    },
+    RIDING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.riding;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isPassenger();
+        }
+    },
+    SPIN_ATTACKING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.spinAttacking;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isAutoSpinAttack();
+        }
+    },
+    USING {
+        @Override
+        boolean isActionEnabled(ClientConfig.DisplayActions displayActions) {
+            return displayActions.usingItem;
+        }
+
+        @Override
+        boolean isPerformingAction(Player player) {
+            return player.isUsingItem();
+        }
+    };
 
     static final DisplayAction[] VALUES = values();
 
-    final Predicate<Player> playerAction;
-    final Predicate<ClientConfig.DisplayActionsConfig> isEnabled;
+    abstract boolean isActionEnabled(ClientConfig.DisplayActions displayActions);
 
-    DisplayAction(Predicate<Player> playerAction, Predicate<ClientConfig.DisplayActionsConfig> isEnabled) {
-        this.playerAction = playerAction;
-        this.isEnabled = isEnabled;
+    abstract boolean isPerformingAction(Player player);
+
+    boolean isPerformingAction(Player player, int remainingRidingTicks) {
+        return remainingRidingTicks == 0 && this.isPerformingAction(player);
     }
 
-    public static boolean isActive(ClientConfig.DisplayActionsConfig config, Player player, int remainingRidingTicks) {
+    public static boolean isActive(ClientConfig.DisplayActions displayActions, Player player, int remainingRidingTicks) {
         for (DisplayAction displayAction : VALUES) {
-            if (displayAction.isEnabled.test(config) && displayAction.isActive(player, remainingRidingTicks)) {
+            if (displayAction.isActionEnabled(displayActions) && displayAction.isPerformingAction(player,
+                    remainingRidingTicks)) {
                 return true;
             }
         }
 
         return false;
-    }
-
-    private boolean isActive(Player player, int remainingRidingTicks) {
-        return (this != CROUCHING || remainingRidingTicks == 0)
-                && this.playerAction.test(player);
     }
 }
